ab84908605d6e83e317e75b8bc307cc3b8e69729
Nguyễn Công Quang <nguyencongquang020803@gmail.com>
2022-02-04 09:21:05 +0700

    Add project files.

---
+++ Main/main.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Threading;
+using System.Data.OleDb;
+using System.Data;
+using System.Text;
+
+namespace Tools
+{
+    internal class main
+    {
+        static void Main(string[] args) // xử lý datava và form
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            system();
+            void system()
+            {
+                try
+                {
+                    form(1);
+                    Console.Write("\t-> ");
+                    int ifsystem = Convert.ToInt32(Console.ReadLine());
+                    switch (ifsystem)
+                    {
+                        case 1:
+                            form(3);
+                            Console.Write("\t-> ");
+                            int dk1 = Convert.ToInt32(Console.ReadLine());
+                            if (dk1 == 1)
+                            {
+                                SLDatabase(1, 1);
+                            }
+                            if (dk1 == 2)
+                            {
+                                SLDatabase(1, 2);
+                            }
+                            system();
+                            break;
+                        case 2:
+                            form(5);
+                            Console.Write("\t-> ");
+                            int dk2 = Convert.ToInt32(Console.ReadLine());
+                            if (dk2 == 1)
+                            {
+                                SLDatabase(2, 1);
+                            }
+                            else if (dk2 == 2)
+                            {
+                                SLDatabase(2, 2);
+                            }
+                            else if (dk2 == 3)
+                            {
+                                SLDatabase(2, 3);
+                            }
+                            else if (dk2 == 4)
+                            {
+                                SLDatabase(2, 4);
+                            }
+                            else if (dk2 == 5)
+                            {
+                                SLDatabase(2, 5);
+                            }
+                            else if (dk2 == 6)
+                            {
+                                SLDatabase(2, 6);
+                            }
+                            system();
+                            break;
+                        default:
+                            form(3);
+                            system();
+                            break;
+                    }
+                }
+                catch (Exception)
+                {
+                    form(2);
+                    Thread.Sleep(TimeSpan.FromSeconds(1));
+                    system();
+                }
+            }
+
+        }
+        static void SLDatabase(int ifo, int ifi) // đưa data vào system
+        {
+            string linkdata = @"D:\Nghề";
+            string data = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + linkdata + @"\Database.accdb";
+
+            try
+            {
+                OleDbConnection conn = new OleDbConnection(data);
+                conn.Open();
+                OleDbCommand sqlcmd = new OleDbCommand();
+
+                sqlcmd.CommandType = CommandType.Text;
+                sqlcmd.CommandText = "SELECT tk.tk, tk.mk FROM tk";
+                sqlcmd.Connection = conn;
+                OleDbDataReader reader = sqlcmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    string tk = reader.GetString(0);
+                    string mk = reader.GetString(1);
+                    Thread t = new Thread(() =>
+                    {
+                        if (ifo == 1)
+                        {
+                            if (ifi == 1)
+                            {
+                                system system = new system(1, tk, mk, "1", 0);
+                            }
+                            else if (ifi == 2)
+                            {
+                                system system = new system(1, tk, mk, "1", 0);
+                            }
+                        }
+                        else if (ifo == 2)
+                        {
+                            if (ifi == 1)
+                            {
+                                system system = new system(2, tk, mk, "1", 1);
+                            }
+                            else if (ifi == 2)
+                            {
+                                system system = new system(2, tk, mk, "1", 2);
+                            }
+                            else if (ifi == 3)
+                            {
+                                system system = new system(2, tk, mk, "1", 3);
+                            }
+                            else if (ifi == 4)
+                            {
+                                system system = new system(2, tk, mk, "1", 4);
+                            }
+                            else if (ifi == 5)
+                            {
+                                system system = new system(2, tk, mk, "1", 5);
+                            }
+                            else if (ifi == 6)
+                            {
+                                system system = new system(2, tk, mk, "1", 6);
+                            }
+                        }
+                    }); t.Start();
+                }
+                conn.Close();
+            }
+            catch (Exception)
+            {
+                form(4);
+            }
+        }
+        static void form(int if1) //  thiết kế form
+        {
+            Console.Title = "form Tools";
+
+            switch (if1)
+            {
+                case 1:
+                    Console.Clear();
+                    Console.WriteLine("-----------------------------------");
+                    Console.WriteLine("| 1: Auto Lướt                    |");
+                    Console.WriteLine("| 2: Auto report                  |");
+                    Console.WriteLine("| 3:                              |");
+                    Console.WriteLine("| 4:                              |");
+                    Console.WriteLine("-----------------------------------");
+                    break;
+                case 2:
+                    Console.Clear();
+                    Console.WriteLine("-----------------------------------");
+                    Console.WriteLine("|            Lỗi                  |");
+                    Console.WriteLine("-----------------------------------");
+                    break;
+                case 3:
+                    Console.Clear();
+                    Console.WriteLine("\t1: Full nick\n\t2: 3 nick");
+                    break;
+                case 4:
+                    Console.Clear();
+                    Console.WriteLine("-----------------------------------");
+                    Console.WriteLine("|     Lỗi kết nối tới database    |");
+                    Console.WriteLine("-----------------------------------");
+                    break;
+                case 5:
+                    Console.Clear();
+                    Console.WriteLine("-----------------------------------");
+                    Console.WriteLine("|            Lỗi                  |");
+                    Console.WriteLine("|            Lỗi                  |");
+                    Console.WriteLine("|            Lỗi                  |");
+                    Console.WriteLine("|            Lỗi                  |");
+                    Console.WriteLine("-----------------------------------");
+                    break;
+                default:
+                    Console.Clear();
+                    Console.WriteLine("Lỗi form");
+                    break;
+            }
+        }
+    }
+}
---
+++ Main/system.cs
@@ -0,0 +1,76 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+using System;
+using System.Drawing;
+using System.Text;
+using System.Threading;
+
+
+namespace Tools
+{
+    internal class system
+    {
+        public system(int if1, string tk, string mk, string Did, int Rid)
+        {
+            var driverService = ChromeDriverService.CreateDefaultService();
+            driverService.HideCommandPromptWindow = true;
+            var driver = new ChromeDriver(driverService, new ChromeOptions());
+            driver.Manage().Window.Size = new Size(300, 490);
+            driver.Navigate().GoToUrl("https://www.facebook.com/");
+            driver.FindElement(By.Id("email")).SendKeys(tk);
+            driver.FindElement(By.Id("pass")).SendKeys(mk + Keys.Return);
+            string llink = $"https://mbasic.facebook.com/ixt/trigger/nfx/msite/?trigger%5Btrigger_event_type%5D=nfx_action_executed&trigger%5Bnfx_context%5D=%7B%22session_id%22%3A%2239d7f8a4-fdbf-4d05-ac44-3e68f705a86b%22%2C%22type%22%3A2%2C%22initial_action_name%22%3A%22RESOLVE_PROBLEM%22%2C%22story_location%22%3A%22profile_someone_else%22%2C%22entry_point%22%3A%22profile_report_button%22%2C%22actions_taken%22%3A%22RESOLVE_PROBLEM%22%2C%22reportable_ent_token%22%3A%22{Did}%22%7D&trigger%5Btrigger_session_id%5D=caeae43f-55f7-449a-b017-308293ee4dd1&_rdr";
+            try
+            {
+                switch (if1)
+                {
+                    case 1:// nuôi fb version 1
+                        Actions action = new Actions(driver);
+                        action.SendKeys(Keys.Escape);
+                        while (true)
+                        {
+                            Random rnd = new Random();
+                            int num = rnd.Next(3000);
+                            action.SendKeys(Keys.Down).Perform();
+                            Thread.Sleep(num);
+                        }
+
+                    case 2:// repost nick fb
+                        switch (Rid)
+                        {
+                            ///html/body/div/div/div/div/table/tbody/tr/td/form/section/fieldset/label[1]/div/table/tbody/tr/td[1]/input
+                            case 1:
+                                Console.WriteLine("1");
+                                break;
+                            case 2:
+                                Console.WriteLine("2");
+                                break;
+                            case 3:
+                                Console.WriteLine("3");
+                                break;
+                            case 4:
+                                Console.WriteLine("4");
+                                break;
+                            case 5:
+                                Console.WriteLine("5");
+                                break;
+                            case 6:
+                                Console.WriteLine("6");
+                                break;
+                            default:
+                                break;
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+    }
+}
